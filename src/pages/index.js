a8e21ae03054980da0f777cc41ad7f2405383e74
RubberDuckDestroyer <hitarthasrani@gmail.com>
2019-07-21 12:36:05 +1200

    Blog link trial
    Trying to link to blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
     <div style={{ color: `purple`}}>
         <h1>Hello Gatsby!</h1>
         <p>What a world!</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <div>
+            <Link to="blog.rubberduckdestroyer.dev">blog</Link>
+        </div>
+
     </div>
   )
